b0438cb084eabba8ce7fef28fe82f211444351cf
OCHOLA EDDYPHIL <eddyphil.ochola@strathmore.edu>
2025-06-15 17:03:54 +0300

    Update InventoryRepository to use Inventory model instead of InventoryItem

--- app/src/main/java/com/example/mwakdasicollection/repositories/InventoryRepository.kt
+++ app/src/main/java/com/example/mwakdasicollection/repositories/InventoryRepository.kt
@@ -1,6 +1,6 @@
 package com.example.mwakdasicollection.repositories
 
-import com.example.mwakdasicollection.model.InventoryItem
+import com.example.mwakdasicollection.model.Inventory
 import com.google.firebase.firestore.FirebaseFirestore
 
 class InventoryRepository {
@@ -8,11 +8,11 @@ class InventoryRepository {
     private val inventoryRef = db.collection("inventory")
 
     // Fetches inventory items from Firestore
-    fun getInventory(onResult: (List<InventoryItem>?, Exception?) -> Unit) {
+    fun getInventory(onResult: (List<Inventory>?, Exception?) -> Unit) {
         inventoryRef.get()
             .addOnSuccessListener { querySnapshot ->
                 val inventory = querySnapshot.documents.mapNotNull { doc ->
-                    doc.toObject(InventoryItem::class.java) // Deserialize Firestore documents into InventoryItem objects
+                    doc.toObject(Inventory::class.java) // Deserialize Firestore documents into InventoryItem objects
                 }
                 onResult(inventory, null) // Return the inventory list
             }
